261cdd6df6aeaaa74bbb5648c1b3740e431cfc5c
AydanLing <118571772+AydanLing@users.noreply.github.com>
2025-12-26 17:02:03 -0500

    IK done!

---
+++ Servos.py
@@ -0,0 +1,39 @@
+import time
+import math
+from newIK import IK
+from servo import Servo, servo2040 # type: ignore
+
+class ServoController:
+    def __init__(self, pin, range, direction):
+        self.pin = pin
+        self.range = 90/(range/2)
+        self.servo = Servo(pin)
+        self.direction = direction
+
+   
+    def rotateAngle(self, angle):
+        if self.direction == "negative":
+            angle = -angle
+        self.servo.value(angle*self.range)
+    @staticmethod
+    def MoveOnCoordinate (angles):
+        theta1,theta2,theta3,theta4 = angles
+        print(angles)
+        turret.rotateAngle(theta1)
+        time.sleep(0.3)
+        pitch1.rotateAngle(theta2)
+        time.sleep(0.3)
+        pitch2.rotateAngle(theta3)
+        time.sleep(0.3)
+        pitch3.rotateAngle(theta4)
+
+turret = ServoController(0, 270,"positive")
+pitch1 = ServoController(1, 270,"positive")
+pitch2 = ServoController(2, 270,"negative")
+pitch3 = ServoController(3, 180,"positive")
+claw = ServoController(4, 180,"positive")
+zero = 0,0,0,0
+straight = 0,90,0,0
+
+ServoController.MoveOnCoordinate(zero)
+time.sleep(10000)
---
+++ newIK.py
@@ -0,0 +1,42 @@
+class IK:
+    @staticmethod
+    def solver(x,y,z):
+        import math
+        base = 127
+        L1 = 120
+        L2 = 120
+        L3 = 80
+
+        if z != 0:
+            theta1 = math.degrees(math.atan(x/z))
+        else:
+            theta1 = 0
+
+        x = math.sqrt(x**2+z**2) # Remap x into 2 dimensions
+
+        x = x - 80 # Apply offset for claw length
+
+        y = y-base # Apply offset for base height
+
+        theta2 = math.degrees(math.atan((240*y)/(240*x)) + math.acos((x**2+y**2)/(240*math.sqrt(x**2+y**2))))
+
+        d = math.sqrt(x**2+y**2)
+
+        theta3 = math.degrees(math.acos((d**2-L1**2-L2**2)/(-2*L1*L2))) + 180
+
+        theta4 = -(theta2+theta3)
+
+        theta4 = theta4 % 360 # Normalize angle into the 1st Quadrant
+
+        if theta2 > 180: # turn reflex (>180) angles into negative angles
+            theta2 = -(360-theta2)
+        if theta3 > 180:
+            theta3 = -(360-theta3)
+        if theta4 > 180:
+            theta4 = -(360-theta4)
+        
+        if abs(theta1) > 135 or abs(theta2) > 135 or abs(theta3) > 135 or abs(theta4) > 90: # Check if angle is out of servo range
+            return 0, 0, 0, 0
+        
+        return theta1,theta2,theta3,theta4
+
